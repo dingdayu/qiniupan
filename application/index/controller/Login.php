a19bbf36e297bb263aceb0432500da3d7a83b856
dingdayu <614422099@qq.com>
2016-06-19 09:50:34 +0800

    init
    Signed-off-by: dingdayu <614422099@qq.com>

---
+++ application/index/controller/Login.php
@@ -0,0 +1,58 @@
+<?php
+// +----------------------------------------------------------------------
+// | DINGDAYU [ WWW.XYSER.COM ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2015 http://dingxiaoyu.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( dingdayu @ XYSER )
+// +----------------------------------------------------------------------
+// | Author: dingdayu <614422099@qq.com>
+// +----------------------------------------------------------------------
+// | DATE: DATE: 2016-6-18 23:07
+// +----------------------------------------------------------------------
+
+
+namespace app\index\controller;
+
+
+use think\Controller;
+
+class Login extends Controller
+{
+    public function _initialize()
+    {
+
+    }
+    public function index()
+    {
+        if(session('?uid')) {
+            return $this->redirect('Index/index');
+        }
+
+        $template = [];
+        $template['__SELF__'] = request()->url();
+        $template['loginUrl'] = url('Login/ajaxLogin');
+        return view('',$template);
+    }
+
+    public function ajaxLogin($email = '', $password = '',$remember = 0)
+    {
+        session('email', 'test@ddy.com');
+        session('uid', '1');
+        if($remember == "true") {
+            $sessionName = ini_get('session.name');
+            $sessionId = session_id();
+            setcookie($sessionName, $sessionId, time() + 3156000);
+            $data = ['code' => 200, 'msg' => 'sucess','url' => url('Index/index')];
+            return json($data);
+        }
+        $data = ['code' => 300, 'msg' => 'erro','url' => url('Index/index')];
+        return json($data);
+    }
+
+    public function quit()
+    {
+        session(null);
+        $this->success('ÍË³ö³É¹¦');
+    }
+}
